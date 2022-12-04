343fca0c4d1264b90584a2da49f6cbfb1abb0a47
muneem <muneem@propertyfinder.ae>
2022-12-04 20:27:35 +0400

    feat: change title to roman numeral calculator

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Roman Numeral calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
